94b180f8445c1c4bc3ff2049e04799735d1614b2
Joaquin Prada <joaquiprada@gmail.com>
2025-04-29 20:36:30 +0000

    astradb refactor implemented

--- src/astrawiki.ts
+++ src/astrawiki.ts
@@ -1,35 +1,34 @@
 import { ArticleInfo } from "./index.js";
-import { startOrbitDb } from "./utils/startOrbitdb.js";
 import { ArticleRepository } from "./articleRepository.js";
-import { AstrawikiNode as AstrawikiNode } from "./index.js";
+import { Astrawiki } from "./index.js";
 import type { Blockstore } from "interface-blockstore";
 import type { Datastore } from "interface-datastore";
+import { AstraDb, createAstraDb } from "@bitxenia/astradb";
 
-export class AstrawikiNodeP2P implements AstrawikiNode {
+export class AstrawikiNode implements Astrawiki {
+  wikiName: string;
+  astraDb: AstraDb;
   articleRepository: ArticleRepository;
 
-  public async start(
-    wikiName: string,
+  constructor(wikiName: string) {
+    this.wikiName = wikiName;
+  }
+
+  public async init(
     isCollaborator: boolean,
     datastore: Datastore,
     blockstore: Blockstore,
     publicIP: string
   ): Promise<void> {
-    if (isCollaborator) {
-      if (!datastore || !blockstore) {
-        throw new Error(
-          "A collaborator node should use a persistent datastore and blockstore."
-        );
-      }
-    }
-    const orbitdb = await startOrbitDb(datastore, blockstore, publicIP);
+    this.astraDb = await createAstraDb({
+      dbName: this.wikiName,
+      isCollaborator: isCollaborator,
+      datastore: datastore,
+      blockstore: blockstore,
+      publicIP: publicIP,
+    });
 
-    this.articleRepository = new ArticleRepository(
-      orbitdb,
-      wikiName,
-      isCollaborator
-    );
-    await this.articleRepository.init();
+    this.articleRepository = new ArticleRepository(this.wikiName, this.astraDb);
   }
 
   public async getArticle(
@@ -65,9 +64,5 @@ export class AstrawikiNodeP2P implements AstrawikiNode {
 
   public async getArticleList(): Promise<string[]> {
     return await this.articleRepository.getArticleList();
-  }
-
-  public async stop(): Promise<void> {
-    // TODO: Implement?
   }
 }
